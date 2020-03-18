4b54357899f51c401e23f9e9713388ccc29df138
KI Serge Laobien <sergelaobienk@gmail.com>
2020-03-18 18:31:08 +0000

    creation PersonnelsTest

--- src/test/java/PGLP_4/Pglp4_1/PersonnelsTest.java
+++ src/test/java/PGLP_4/Pglp4_1/PersonnelsTest.java
@@ -1,5 +1,12 @@
 package PGLP_4.Pglp4_1;
 
 public class PersonnelsTest {
+    Personnels perso1;
+    Personnels perso2;
+    Personnels perso3;
+    Personnels perso4;
 
+    Composite grp1;
+    Composite grp2;
+    Composite grp3;
 }
